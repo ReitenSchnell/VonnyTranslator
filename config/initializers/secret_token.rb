5bdd2e6c9757828848cdfd680c45c70fa940403f
Reiten Schell <alina542.1@gmail.com>
2012-09-24 22:32:11 +0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VonnyTranslator::Application.config.secret_token = 'a2e9b9d308d24c176226f0278df3c87086a38db9c6d793f4bac2693c529c3b75a522da6c67951db16e2b9b7fe0ac867437d66a0316419c7f1c455d1c13c9a1c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+VonnyTranslator::Application.config.session_store :cookie_store, key: '_VonnyTranslator_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# VonnyTranslator::Application.config.session_store :active_record_store
