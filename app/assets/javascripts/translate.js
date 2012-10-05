a8f176511b7de9c63d67a4c7472a91c834b7089a
Alina <alina542.1@gmail.com>
2012-10-05 13:51:39 +0300

    nonvowels removed

--- app/assets/javascripts/translate.js
+++ app/assets/javascripts/translate.js
@@ -6,14 +6,58 @@ translate = function(str) {
                         "сова" : "Своа",
                         "своа" : "Своа",
                         "курорт" : "крорт",
-                        "поехали" : "пехли" };
+                        "поехали" : "пехли",
+                        "и" : "и",
+                        "на" : "на",
+                        "я" : "я" };
   var words = str.toLowerCase().split(' ');
   for (var i = 0; i < words.length; i++)
   {
-      if (words[i] in static_names)
-        {
-            words[i] = static_names[words[i]];
-        }
+      var word = words[i];
+      var lastChar = word[word.length - 1];
+      if (!isConsonant(lastChar) && !isVowel(lastChar))
+      {
+         word = word.substr(0,word.length - 1)
+      }
+      else
+      {
+         lastChar = ''
+      }
+      if (word in static_names)
+      {
+         word = static_names[word];
+      }
+      else
+      {
+          var newWord = '';
+          for (var j = 0; j < word.length; j++)
+          {
+              if (!isVowel(word[j]))
+              {
+                  newWord = newWord.concat(word[j]);
+              }
+          }
+          word = newWord;
+      }
+      words[i] =  word + lastChar;
   }
   return words.join(' ')
 };
+
+isVowel = function(char)
+{
+    if (char.length == 1)
+    {
+        var vowels = "аеёиоуэюяы";
+        return vowels.indexOf(char) >= 0 ? true : false;
+    }
+}
+
+isConsonant = function(char)
+{
+    if (char.length == 1)
+    {
+        var consonants = "бвгджзйклмнпрстфхцчшщьъ";
+        return consonants.indexOf(char) >= 0 ? true : false;
+    }
+}
--- spec/javascripts/translate_spec.js
+++ spec/javascripts/translate_spec.js
@@ -10,4 +10,38 @@ describe('translate', function() {
       translate("потачок и пятачок").should.equal("Потачок и Потачок");
       translate("Сова поехали на курорт").should.equal("Своа пехли на крорт");
   });
+
+  it('should translate sentences with punctuation', function(){
+      translate("Винни, Пятачок").should.equal("Вонни, Потачок");
+      translate("потачок и пятачок.").should.equal("Потачок и Потачок.");
+      translate("Сова, поехали на курорт").should.equal("Своа, пехли на крорт");
+  });
+
+    it('should remove all vowels', function(){
+        translate("Сова, я тебе говорю, поехали на курорт").should.equal("Своа, я тб гвр, пехли на крорт");
+        translate("Ты принес?").should.equal("т прнс?");
+    });
+});
+
+describe('isVowel', function() {
+    it('should recognise vowels', function() {
+        isVowel('а').should.be.true;
+        isVowel('э').should.be.true;
+    });
+    it('should not recognise nonvowels', function() {
+        isVowel('б').should.be.false;
+        isVowel('ц').should.be.false;
+        isVowel(',').should.be.false;
+    });
+});
+
+describe('isConsonant', function() {
+    it('should recognise consonants', function() {
+        isConsonant('б').should.be.true;
+        isConsonant('ц').should.be.true;
+    });
+    it('should not recognise non punctuation marks', function() {
+        isConsonant(',').should.be.false;
+        isConsonant('а').should.be.false;
+    });
 });
